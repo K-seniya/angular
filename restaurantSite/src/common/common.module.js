2bfe256444fa8a1ceab70ed9717e093777df044c
Ksenia <noEmail@example.com>
2017-01-11 22:51:30 +0100

    interceptors

--- restaurantSite/src/common/common.module.js
+++ restaurantSite/src/common/common.module.js
@@ -1,5 +1,11 @@
 (function () {
   "use strict";
 
-  angular.module('common', []);
+  angular.module('common', [])
+  .config(config);
+
+  config.$inject = ['$httpProvider'];
+  function config($httpProvider) {
+    $httpProvider.interceptors.push('loadingHttpInterceptor');
+  }
 })();
