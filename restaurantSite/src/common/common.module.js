64247034166ebfb1282e2383e74c766d389ee04e
Ksenia <noEmail@example.com>
2017-01-15 22:09:00 +0100

    MenuCategories. part 1

--- restaurantSite/src/common/common.module.js
+++ restaurantSite/src/common/common.module.js
@@ -2,6 +2,7 @@
   "use strict";
 
   angular.module('common', [])
+  .constant('ApiPath', 'https://ychaikin-course5.herokuapp.com')
   .config(config);
 
   config.$inject = ['$httpProvider'];
--- restaurantSite/src/public/public.routes.js
+++ restaurantSite/src/public/public.routes.js
@@ -15,6 +15,17 @@
     .state('public.home', {
       url: '/',
       templateUrl: 'src/public/home/home.html'
+    })
+    .state('public.menu', {
+      url: '/menu',
+      templateUrl: 'src/public/menu/menu.html',
+      controller: 'MenuController',
+      controllerAs: 'menuCtrl',
+      resolve: {
+        menuCategories: ['MenuService', function (MenuService) {
+          return MenuService.getCategories();
+        }]
+      }
     });
 
   }
